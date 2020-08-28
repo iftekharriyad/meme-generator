8f9f1827ae5e84daba3bb001e85dd1d92f9efd09
iftekharriyad <iftekharriyad@protonmail.com>
2020-08-28 16:53:09 +0600

    meme generator is working now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Header from './components/Header'
+import MemeGenerator from './components/MemeGenerator'
+
 import './App.css';
 
 function App() {
   return (
       <div>
-
+        <Header/>
+        <MemeGenerator/>
       </div>
   );
 }
